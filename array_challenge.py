4946ae4e503abe73d849477f955b07e4436f00a5
Abhishek Pandey <abhishekpandey@Abhisheks-MacBook-Pro.local>
2021-04-06 19:08:57 +0530

    minor-change

--- array_challenge.py
+++ array_challenge.py
@@ -1,9 +1,9 @@
 from itertools import combinations
-a = [22, 6, 23, 10, 1, 3]
+a = [1,2,3,6,8,21]
 m = max(a)
 a.remove(m)
 def check(a):
-    for i in range(1,len(a)):
+    for i in range(1,len(a)+1):
         l = [sum(j) for j in combinations(a,i)]
         if m in l:
             return True
